33274ddf1713068440439cc77c83d91919744d50
chuangeng555 <psp.chuangeng@gmail.com>
2021-10-13 20:01:41 +0800

    event table UI with sort, search, pagination and buttons

---
+++ beepbeep/public/js/services/eventService.js
@@ -0,0 +1,359 @@
+//temp data
+
+let eventData = {
+  eventName: 'eventTest',
+  dateTime: '2021-10-14T03:08:00.000Z',
+  duration: 90,
+  attendees: {
+    'eventTest-David': {
+      attendeeName: 'David',
+      round1Attendance: false,
+      round2Attendance: false,
+      round1Temperature: 0,
+      round2Temperature: 0
+    },
+    'eventTest-John': {
+      attendeeName: 'John',
+      round1Attendance: false,
+      round2Attendance: false,
+      round1Temperature: 0,
+      round2Temperature: 0
+    },
+    'eventTest-Paul': {
+      attendeeName: 'Paul',
+      round1Attendance: false,
+      round2Attendance: false,
+      round1Temperature: 0,
+      round2Temperature: 0
+    },
+    'eventTest-Mark': {
+      attendeeName: 'Mark',
+      round1Attendance: false,
+      round2Attendance: false,
+      round1Temperature: 0,
+      round2Temperature: 0
+    },
+    'eventTest-James': {
+      attendeeName: 'James',
+      round1Attendance: false,
+      round2Attendance: false,
+      round1Temperature: 0,
+      round2Temperature: 0
+    },
+    'eventTest-Andrew': {
+      attendeeName: 'Andrew',
+      round1Attendance: false,
+      round2Attendance: false,
+      round1Temperature: 0,
+      round2Temperature: 0
+    },
+    'eventTest-Scott': {
+      attendeeName: 'Scott',
+      round1Attendance: false,
+      round2Attendance: false,
+      round1Temperature: 0,
+      round2Temperature: 0
+    },
+    'eventTest-Steven': {
+      attendeeName: 'Steven',
+      round1Attendance: false,
+      round2Attendance: false,
+      round1Temperature: 0,
+      round2Temperature: 0
+    },
+    'eventTest-Robert': {
+      attendeeName: 'Robert',
+      round1Attendance: false,
+      round2Attendance: false,
+      round1Temperature: 0,
+      round2Temperature: 0
+    },
+    'eventTest-Stephen': {
+      attendeeName: 'Stephen',
+      round1Attendance: false,
+      round2Attendance: false,
+      round1Temperature: 0,
+      round2Temperature: 0
+    },
+    'eventTest-William': {
+      attendeeName: 'William',
+      round1Attendance: false,
+      round2Attendance: false,
+      round1Temperature: 0,
+      round2Temperature: 0
+    },
+    'eventTest-Craig': {
+      attendeeName: 'Craig',
+      round1Attendance: false,
+      round2Attendance: false,
+      round1Temperature: 0,
+      round2Temperature: 0
+    },
+    'eventTest-Michael': {
+      attendeeName: 'Michael',
+      round1Attendance: false,
+      round2Attendance: false,
+      round1Temperature: 0,
+      round2Temperature: 0
+    },
+    'eventTest-Stuart': {
+      attendeeName: 'Stuart',
+      round1Attendance: false,
+      round2Attendance: false,
+      round1Temperature: 0,
+      round2Temperature: 0
+    },
+    'eventTest-Christopher': {
+      attendeeName: 'Christopher',
+      round1Attendance: false,
+      round2Attendance: false,
+      round1Temperature: 0,
+      round2Temperature: 0
+    },
+    'eventTest-Alan': {
+      attendeeName: 'Alan',
+      round1Attendance: false,
+      round2Attendance: false,
+      round1Temperature: 0,
+      round2Temperature: 0
+    },
+    'eventTest-Colin': {
+      attendeeName: 'Colin',
+      round1Attendance: false,
+      round2Attendance: false,
+      round1Temperature: 0,
+      round2Temperature: 0
+    },
+    'eventTest-Brian': {
+      attendeeName: 'Brian',
+      round1Attendance: false,
+      round2Attendance: false,
+      round1Temperature: 0,
+      round2Temperature: 0
+    },
+    'eventTest-Kevin': {
+      attendeeName: 'Kevin',
+      round1Attendance: false,
+      round2Attendance: false,
+      round1Temperature: 0,
+      round2Temperature: 0
+    },
+    'eventTest-Gary': {
+      attendeeName: 'Gary',
+      round1Attendance: false,
+      round2Attendance: false,
+      round1Temperature: 0,
+      round2Temperature: 0
+    },
+    'eventTest-Richard': {
+      attendeeName: 'Richard',
+      round1Attendance: false,
+      round2Attendance: false,
+      round1Temperature: 0,
+      round2Temperature: 0
+    },
+    'eventTest-Derek': {
+      attendeeName: 'Derek',
+      round1Attendance: false,
+      round2Attendance: false,
+      round1Temperature: 0,
+      round2Temperature: 0
+    },
+    'eventTest-Martin': {
+      attendeeName: 'Martin',
+      round1Attendance: false,
+      round2Attendance: false,
+      round1Temperature: 0,
+      round2Temperature: 0
+    },
+    'eventTest-Thomas': {
+      attendeeName: 'Thomas',
+      round1Attendance: false,
+      round2Attendance: false,
+      round1Temperature: 0,
+      round2Temperature: 0
+    },
+    'eventTest-Neil': {
+      attendeeName: 'Neil',
+      round1Attendance: false,
+      round2Attendance: false,
+      round1Temperature: 0,
+      round2Temperature: 0
+    },
+    'eventTest-Barry': {
+      attendeeName: 'Barry',
+      round1Attendance: false,
+      round2Attendance: false,
+      round1Temperature: 0,
+      round2Temperature: 0
+    },
+    'eventTest-Ian': {
+      attendeeName: 'Ian',
+      round1Attendance: false,
+      round2Attendance: false,
+      round1Temperature: 0,
+      round2Temperature: 0
+    },
+    'eventTest-Jason': {
+      attendeeName: 'Jason',
+      round1Attendance: false,
+      round2Attendance: false,
+      round1Temperature: 0,
+      round2Temperature: 0
+    },
+    'eventTest-Iain': {
+      attendeeName: 'Iain',
+      round1Attendance: false,
+      round2Attendance: false,
+      round1Temperature: 0,
+      round2Temperature: 0
+    },
+    'eventTest-Gordon': {
+      attendeeName: 'Gordon',
+      round1Attendance: false,
+      round2Attendance: false,
+      round1Temperature: 0,
+      round2Temperature: 0
+    },
+    'eventTest-Alexander': {
+      attendeeName: 'Alexander',
+      round1Attendance: false,
+      round2Attendance: false,
+      round1Temperature: 0,
+      round2Temperature: 0
+    },
+    'eventTest-Graeme': {
+      attendeeName: 'Graeme',
+      round1Attendance: false,
+      round2Attendance: false,
+      round1Temperature: 0,
+      round2Temperature: 0
+    },
+    'eventTest-Peter': {
+      attendeeName: 'Peter',
+      round1Attendance: false,
+      round2Attendance: false,
+      round1Temperature: 0,
+      round2Temperature: 0
+    },
+    'eventTest-Darren': {
+      attendeeName: 'Darren',
+      round1Attendance: false,
+      round2Attendance: false,
+      round1Temperature: 0,
+      round2Temperature: 0
+    },
+    'eventTest-Graham': {
+      attendeeName: 'Graham',
+      round1Attendance: false,
+      round2Attendance: false,
+      round1Temperature: 0,
+      round2Temperature: 0
+    },
+    'eventTest-George': {
+      attendeeName: 'George',
+      round1Attendance: false,
+      round2Attendance: false,
+      round1Temperature: 0,
+      round2Temperature: 0
+    },
+    'eventTest-Kenneth': {
+      attendeeName: 'Kenneth',
+      round1Attendance: false,
+      round2Attendance: false,
+      round1Temperature: 0,
+      round2Temperature: 0
+    },
+    'eventTest-Allan': {
+      attendeeName: 'Allan',
+      round1Attendance: false,
+      round2Attendance: false,
+      round1Temperature: 0,
+      round2Temperature: 0
+    },
+    'eventTest-Simon': {
+      attendeeName: 'Simon',
+      round1Attendance: false,
+      round2Attendance: false,
+      round1Temperature: 0,
+      round2Temperature: 0
+    },
+    'eventTest-Douglas': {
+      attendeeName: 'Douglas',
+      round1Attendance: false,
+      round2Attendance: false,
+      round1Temperature: 0,
+      round2Temperature: 0
+    },
+    'eventTest-Keith': {
+      attendeeName: 'Keith',
+      round1Attendance: false,
+      round2Attendance: false,
+      round1Temperature: 0,
+      round2Temperature: 0
+    },
+    'eventTest-Lee': {
+      attendeeName: 'Lee',
+      round1Attendance: false,
+      round2Attendance: false,
+      round1Temperature: 0,
+      round2Temperature: 0
+    },
+    'eventTest-Anthony': {
+      attendeeName: 'Anthony',
+      round1Attendance: false,
+      round2Attendance: false,
+      round1Temperature: 0,
+      round2Temperature: 0
+    },
+    'eventTest-Grant': {
+      attendeeName: 'Grant',
+      round1Attendance: false,
+      round2Attendance: false,
+      round1Temperature: 0,
+      round2Temperature: 0
+    },
+    'eventTest-Ross': {
+      attendeeName: 'Ross',
+      round1Attendance: false,
+      round2Attendance: false,
+      round1Temperature: 0,
+      round2Temperature: 0
+    },
+    'eventTest-Jonathan': {
+      attendeeName: 'Jonathan',
+      round1Attendance: false,
+      round2Attendance: false,
+      round1Temperature: 0,
+      round2Temperature: 0
+    },
+    'eventTest-Gavin': {
+      attendeeName: 'Gavin',
+      round1Attendance: false,
+      round2Attendance: false,
+      round1Temperature: 0,
+      round2Temperature: 0
+    },
+    'eventTest-Nicholas': {
+      attendeeName: 'Nicholas',
+      round1Attendance: false,
+      round2Attendance: false,
+      round1Temperature: 0,
+      round2Temperature: 0
+    },
+    'eventTest-Joseph': {
+      attendeeName: 'Joseph',
+      round1Attendance: false,
+      round2Attendance: false,
+      round1Temperature: 0,
+      round2Temperature: 0
+    },
+    'eventTest-Stewart': {
+      attendeeName: 'Stewart',
+      round1Attendance: false,
+      round2Attendance: false,
+      round1Temperature: 0,
+      round2Temperature: 0
+    }
+  }
+}
