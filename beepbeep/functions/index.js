88b84d157fe321091338d1f0002ef9fde414034c
Josh Tseng <joshua.tseng.sh@gmail.com>
2021-10-15 01:07:50 +0800

    Added functions in index.js
    - addEvent function
    - getAllEvents function
    - Does not do error handling yet

--- beepbeep/functions/index.js
+++ beepbeep/functions/index.js
@@ -1,23 +1,85 @@
-// const functions = requre("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const functions = require('firebase-functions');
+const admin = require("firebase-admin");
 
-// Initialize Cloud Firestore through Firebase
-// import { initializeApp } from "firebase/app"
-// import { getFirestore } from "firebase/firestore"
+const cors = require('cors');
+const express = require('express');
+const app = express();
 
-// const firebaseApp = initializeApp({
-//     apiKey: "AIzaSyBNQ2ZRxFYd7I42vZZRhFPfqwa4tpKr3ZI",
-//     authDomain: "beepbeep-45b71.firebaseapp.com",
-//     databaseURL: "https://beepbeep-45b71-default-rtdb.asia-southeast1.firebasedatabase.app",
-//     projectId: "beepbeep-45b71",
-//     storageBucket: "beepbeep-45b71.appspot.com",
-//     messagingSenderId: "640515235677",
-//     appId: "1:640515235677:web:bce41eb1784d6259b692b4"
-// });
+//const serviceAccount = require("./beepbeep-key.json");
+
+admin.initializeApp();
+
+const firestore = admin.firestore();
+
+// Automatically allow cross-origin requests
+app.use(cors({ origin: "*" }));
+
+// Expose Express API as a single Cloud Function:
+exports.widgets = functions.https.onRequest(app);
+
+// build multiple CRUD interfaces:
+app.get('/:id', (req, res) => res.send(Widgets.getById(req.params.id)));
+app.post('/', (req, res) => res.send(Widgets.create()));
+
+// Adds new event
+exports.addEvent = functions.https.onRequest( (req, res) => {
+    //console.log(req.query);
+    const eventName = req.query.eventName;
+    const startDateTime = req.query.startDateTime;
+    const duration = req.query.duration;
+    const attendees = req.query.attendees;
+    
+    //console.log(typeof attendees);
+    attendeesList = [];
+    
+    // Put attendees in associative array (JS object)
+    for (id in attendees) {
+        var attendeeEntry = {
+            attendeeId: id,
+            attendeeName: attendees[id],
+            round1Attendance: false,
+            round2Attendance: false,
+            round1Temperature: null,
+            round2Temperature: null,
+        };
+        attendeesList.push(attendeeEntry);
+    }
+    
+    console.log(attendeesList);
+    
+    // Push the new entry into Firestore using the Firebase Admin SDK
+    res.set('Access-Control-Allow-Origin', '*');
+    const writeResult = admin.firestore().collection("events").add({
+      eventName: eventName,
+      startDateTime: startDateTime,
+      duration: duration,
+      attendees: attendeesList,
+    });
+    
+    console.log("Uploading...");
+    
+    // Send back a message that we've successfully written to db
+    res.json({result: `Message with ID: ${writeResult.id} added.`});
+});
+
+// Get all events
+exports.getAllEvents = functions.https.onRequest( (req, res) => {
+    output = [];
+    
+    firestore.collection("events").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        row = {
+            eventId: doc.id,
+            eventData: doc.data(),
+            //doc.id, " => ", doc.data();
+        }
+        //console.log(row);
+        output.push(row);
+    });
+    });
+    
+    console.log("Sending to client...");
+    res.json(output);
+});
