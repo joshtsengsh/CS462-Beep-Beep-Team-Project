cfa0c7eef371c97c7e1564dd6022aa0d95c11809
jenleng <jen.leng.2019@scis.smu.edu.sg>
2021-11-02 21:40:22 +0800

    Update chartService.js

--- beepbeep/public/js/services/chartService.js
+++ beepbeep/public/js/services/chartService.js
@@ -1,34 +1,34 @@
 
-  // Setup - The visuals and dataset
-  const pieData = {
+// Setup - The visuals and dataset
+const pieData = {
     labels: [
-      'Red',
-      'Blue',
-      'Yellow'
+        'Red',
+        'Blue',
+        'Yellow'
     ],
     datasets: [{
-      label: 'My First Dataset',
-      data: [300, 50, 100],
-      backgroundColor: [
-        'rgb(255, 99, 132)',
-        'rgb(54, 162, 235)',
-        'rgb(255, 205, 86)'
-      ],
-      hoverOffset: 4
+        label: 'My First Dataset',
+        data: [300, 50, 100],
+        backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)',
+            'rgb(255, 205, 86)'
+        ],
+        hoverOffset: 4
     }]
-  };
+};
 
 // Config
 const pieChartconfig = {
     type: 'pie',
     data: pieData,
-  };
-  
-  // Display the chart
-  const pieChart = new Chart(
+};
+
+// Display the chart
+const pieChart = new Chart(
     document.getElementById('pieChart'),
     pieChartconfig
-  );
+);
 
- 
+
 
